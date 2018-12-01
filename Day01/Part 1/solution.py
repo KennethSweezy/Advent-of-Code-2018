72b59e4835c4b3a8981f5a9e17b50a7e258f36a7
KennethSweezy <KennethSweezy@gmail.com>
2018-12-01 11:30:59 -0500

    Create solution.py
    Created solution.py
    Used solution.py to find answer
    Answer: 411
    Status: Correct

---
+++ Day01/Part 1/solution.py
@@ -0,0 +1,15 @@
+total = 0
+
+input = open("C:\Users\Kenneth\Documents\GitHub\Advent-of-Code-2018\Day01\Part 1\input.txt", "r")
+for line in input:
+    if "+" in line:
+        currentLine = line
+        truncatedLine = line[1:]
+        lineVal = int(truncatedLine)
+        total = total + lineVal
+    elif "-" in line:
+        currentLine = line
+        truncatedLine = line[1:]
+        lineVal = int(truncatedLine)
+        total = total - lineVal
+print(total)
