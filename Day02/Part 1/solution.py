ebbb83f541c729a0ce30a989b788a143784027fc
KennethSweezy <KennethSweezy@gmail.com>
2018-12-02 18:12:07 -0500

    Add input.txt and started work on a solution for Part 1

---
+++ Day02/Part 1/solution.py
@@ -0,0 +1,8 @@
+input = open("C:\Users\Kenneth\Documents\GitHub\Advent-of-Code-2018\Day02\Part 1\input.txt", "r")
+
+for line in input:
+
+def charCount(string):
+    for char in string:
+        array_of_chars = []
+        array_of_chars.append(char)
